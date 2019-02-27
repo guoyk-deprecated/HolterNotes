2a7745a16d4f179a222971bb5e67cb4328306d7c
Yanke Guo <guoyk.cn@gmail.com>
2019-02-27 15:33:36 +0800

    add basic structure

---
+++ HolterNotes/AboutViewController.swift
@@ -0,0 +1,11 @@
+//
+//  AboutViewController.swift
+//  HolterNotes
+//
+//  Created by Yanke Guo on 2019/2/27.
+//  Copyright © 2019 Yanke Guo. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {}
